b954ea9de08cef305deeda9817729d9a93ac2d50
angeloper <minupurer@gmail.com>
2022-01-08 00:07:53 +0900

    inital commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### ßangeloper Blog
---
+++ gatsby-meta-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: `my developer history`,
+  description: ``,
+  author: `angeloper`,
+  introduction: `개발이 재미있어 살맛난다.`,
+  siteUrl: `https://kskmw2000.github.io`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `kskmw2000`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'kskmw2000/kskmw2000.github.io', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+  ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
+
+}
